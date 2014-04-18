0fd6fb8b7500c1580102a90e78388e2df8bffedf
Wei-Cheng Pan <legnaleurc@gmail.com>
2014-04-18 11:53:18 +0800

    Fix for PEP8.

--- api.py
+++ api.py
@@ -1,31 +1,29 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-"""
-Copyright (c) 2014 Wei-Cheng Pan (潘韋成) <legnaleurc@gmail.com>
+# Copyright (c) 2014 Wei-Cheng Pan (潘韋成) <legnaleurc@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-"""
-
-
-import requests
 
 import json
+
+import requests
 
 
 class API(object):
@@ -34,10 +32,16 @@ class API(object):
         self._api_token = api_token
         self._api_verno = 1
         self._server_prefix = 'http://125.6.189.135'
-        self._referer_port = '{0}/kcs/port.swf?version={1}'.format(self._server_prefix, '1.7.1')
-        self._referer_battle = '{0}/kcs/battle.swf?version={1}'.format(self._server_prefix, '1.4.4')
+        self._referer_port = '{0}/kcs/port.swf?version={1}'.format(
+            self._server_prefix,
+            '1.7.1')
+        self._referer_battle = '{0}/kcs/battle.swf?version={1}'.format(
+            self._server_prefix,
+            '1.4.4')
 
-        self._user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:28.0) Gecko/20100101 Firefox/28.0'
+        self._user_agent = ('Mozilla/5.0'
+                            ' (Macintosh; Intel Mac OS X 10.9; rv:28.0)'
+                            ' Gecko/20100101 Firefox/28.0')
 
     def _do_request(self, path, data=None):
         headers = {
@@ -52,7 +56,10 @@ class API(object):
         if data:
             data_.update(data)
 
-        response = requests.post(self._server_prefix + path, data=data_, headers=headers)
+        response = requests.post(
+            self._server_prefix + path,
+            data=data_,
+            headers=headers)
         if response.status_code != 200:
             return None
 
--- db.py
+++ db.py
@@ -1,25 +1,24 @@
-#-*- coding: utf-8 -*-
-"""
-Copyright (c) 2014 Wei-Cheng Pan (潘韋成) <legnaleurc@gmail.com>
+# -*- coding: utf-8 -*-
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-"""
+# Copyright (c) 2014 Wei-Cheng Pan (潘韋成) <legnaleurc@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 
 import sqlalchemy as sql
@@ -65,7 +64,10 @@ class Ship(_Base):
 
     api_id = sql.Column(sql.Integer, primary_key=True)
     api_sortno = sql.Column(sql.Integer)
-    api_ship_id = sql.Column(sql.Integer, sql.ForeignKey('ship_type.api_id'), nullable=False)
+    api_ship_id = sql.Column(
+        sql.Integer,
+        sql.ForeignKey('ship_type.api_id'),
+        nullable=False)
     api_lv = sql.Column(sql.Integer)
     api_nowhp = sql.Column(sql.Integer)
     api_maxhp = sql.Column(sql.Integer)
